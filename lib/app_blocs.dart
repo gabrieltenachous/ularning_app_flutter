7deea650579278ecccecf7f03a220273c0585858
Gabriel Oliveira Fonseca de Sousa <gabrielbromo@outlook.com>
2023-09-17 20:04:57 -0300

    FIX: add InitState in AppStat

--- lib/app_blocs.dart
+++ lib/app_blocs.dart
@@ -3,7 +3,7 @@ import 'package:ulearning_app/app_events.dart';
 import 'package:ulearning_app/app_states.dart';
 
 class AppBlocs extends Bloc<AppEvents, AppStates> {
-  AppBlocs() : super(InitStates()) {
+  AppBlocs() : super() {
     on<Increment>((event, emit) {
       emit(
         AppStates(counter: state.counter + 1),
