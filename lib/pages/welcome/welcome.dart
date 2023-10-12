ec1beb859b8c2219c0a0291cc29f3323a1be550f
Gabriel Oliveira Fonseca de Sousa <gabrielbromo@outlook.com>
2023-10-12 19:08:43 -0300

    imporstanto assets, bloc funcionando normalmente

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,6 +1,10 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:ulearning_app/pages/welcome/bloc/welcome_events.dart';
+import 'package:ulearning_app/pages/welcome/bloc/welcome_states.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -14,240 +18,162 @@ class _WelcomeState extends State<Welcome> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Scaffold(
-        body: Container(
-          margin: EdgeInsets.only(
-            top: 34.h,
-          ),
-          width: 375.w,
-          child: Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              PageView(
-                children: [
-                  // PAGE VIEW 1
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 345.w,
-                        height: 345.w,
-                        child: const Text(
-                          "Image one",
-                        ),
+      child: Scaffold(body: BlocBuilder<WelcomeBloc, WelcomeState>(
+        builder: (context, state) {
+          return Container(
+            margin: EdgeInsets.only(
+              top: 34.h,
+            ),
+            width: 375.w,
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                PageView(
+                  onPageChanged: (index) {
+                    state.page = index;
+                    BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
+                  },
+                  children: [
+                    _page(
+                      index: 0,
+                      context: context,
+                      buttonName: "next",
+                      imagePath: "image path",
+                      subTitle:
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dumm",
+                      title: "First see Learning",
+                    ),
+                    _page(
+                      index: 1,
+                      context: context,
+                      buttonName: "next",
+                      imagePath: "image path",
+                      subTitle:
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dumm",
+                      title: "First see Learning",
+                    ),
+                    _page(
+                      index: 1,
+                      context: context,
+                      buttonName: "next",
+                      imagePath: "image path",
+                      subTitle:
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dumm",
+                      title: "First see Learning",
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: 100.h,
+                  child: DotsIndicator(
+                    position: state.page,
+                    dotsCount: 3,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    decorator: DotsDecorator(
+                      color: Colors.grey,
+                      size: const Size.square(8.0),
+                      activeSize: const Size(18.0, 8.0),
+                      activeColor: Colors.blue,
+                      activeShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
                       ),
-                      Container(
-                        child: Text(
-                          "First See Learning",
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(
-                          left: 30.w,
-                          right: 30.w,
-                        ),
-                        child: Text(
-                          "Forge about a for of paper all knowledge in one learning",
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(
-                          top: 100.h,
-                          left: 25.w,
-                          right: 25.w,
-                        ),
-                        width: 325.w,
-                        height: 50.h,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.all(Radius.circular(15.w)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 1,
-                              blurRadius: 2,
-                              offset: Offset(0, -1),
-                            )
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Next",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.sp,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  // PAGE VIEW 2
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 345.w,
-                        height: 345.w,
-                        child: const Text(
-                          "Image one",
-                        ),
-                      ),
-                      Container(
-                        child: Text(
-                          "First See Learning",
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(
-                          left: 30.w,
-                          right: 30.w,
-                        ),
-                        child: Text(
-                          "Forge about a for of paper all knowledge in one learning",
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(
-                          top: 100.h,
-                          left: 25.w,
-                          right: 25.w,
-                        ),
-                        width: 325.w,
-                        height: 50.h,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.all(Radius.circular(15.w)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 1,
-                              blurRadius: 2,
-                              offset: Offset(0, -1),
-                            )
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Next",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.sp,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  // PAGE VIEW 3
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 345.w,
-                        height: 345.w,
-                        child: const Text(
-                          "Image one",
-                        ),
-                      ),
-                      Container(
-                        child: Text(
-                          "First See Learning",
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(
-                          left: 30.w,
-                          right: 30.w,
-                        ),
-                        child: Text(
-                          "Forge about a for of paper all knowledge in one learning",
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(
-                          top: 100.h,
-                          left: 25.w,
-                          right: 25.w,
-                        ),
-                        width: 325.w,
-                        height: 50.h,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.all(Radius.circular(15.w)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 1,
-                              blurRadius: 2,
-                              offset: Offset(0, -1),
-                            )
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Next",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.sp,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              Positioned(
-                bottom: 100.h,
-                child: DotsIndicator(
-                  dotsCount: 3,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  decorator: DotsDecorator(
-                    color: Colors.grey,
-                    size: const Size.square(8.0),
-                    activeSize: const Size(10.0, 8.0),
-                    activeColor: Colors.blue,
-                    activeShape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
                     ),
                   ),
                 ),
+              ],
+            ),
+          );
+        },
+      )),
+    );
+  }
+}
+
+class _page extends StatelessWidget {
+  final int index;
+  final BuildContext context;
+  final String buttonName;
+  final String title;
+  final String subTitle;
+  final String imagePath;
+
+  const _page({
+    super.key,
+    required this.index,
+    required this.context,
+    required this.buttonName,
+    required this.title,
+    required this.subTitle,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 345.w,
+          height: 345.w,
+          child: Text(
+            imagePath,
+          ),
+        ),
+        Container(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.5),
+              fontSize: 24.sp,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(
+            left: 30.w,
+            right: 30.w,
+          ),
+          child: Text(
+            subTitle,
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.5),
+              fontSize: 14.sp,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(
+            top: 100.h,
+            left: 25.w,
+            right: 25.w,
+          ),
+          width: 325.w,
+          height: 50.h,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.all(Radius.circular(15.w)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: const Offset(0, -1),
               )
             ],
           ),
+          child: Center(
+            child: Text(
+              buttonName,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
